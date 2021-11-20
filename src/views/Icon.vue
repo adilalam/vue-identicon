6fa4540e07bf6d9feb5e2d60ad577ef77e8a982c
MD ADIL ALAM <16951253+adilalam@users.noreply.github.com>
2021-11-20 13:34:18 +0530

    first commit

---
+++ src/views/Icon.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>My Identicon Generator</h1>
+
+    <div>
+      Input:
+      <input type="text" @input="onInput" />
+    </div>
+
+    <div>
+      Output:
+      <div id="placeholder"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+const jdenticon = require('jdenticon');
+
+export default {
+  name: 'Icon',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    onInput(e) {
+      const placeholder = document.getElementById('placeholder');
+      placeholder.innerHTML = jdenticon.toSvg(e.target.value, 200);
+    },
+  },
+};
+</script>
